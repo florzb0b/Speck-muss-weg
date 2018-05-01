9cf9e4f72b5fdcd52ecf5bc828f44c5486270098
florzb0b <florz1303@googlemail.com>
2018-05-01 16:11:45 +0200

    das ist mein erstes commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package florzb0b.de.testprojectforgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
